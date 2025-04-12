9fd0d5b75f37e450b3843dd5f325f9f6fcc94722
Simon Bergeron <simon.bergeron.home@gmail.com>
2025-04-12 16:19:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from utils import record_video, AskMultipleChoice
+from utils import record_video, ask_multiple_choice
 
 def main():
 	while True:
 		choices = {
-			'Record video': record_video
+			'Record video': record_video,
+			'Review feedback': None,
+			'Exit': None
 		} # More will be added if needed later on
 
 		command = ask_multiple_choice(choices) # input: array, output: command
